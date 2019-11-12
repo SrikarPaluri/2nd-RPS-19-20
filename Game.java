74236af3742820c229ab79e6b8857f10bf568697
Srikar Paluri <57012368+SrikarPaluri@users.noreply.github.com>
2019-11-12 09:03:15 -0800

    final

--- Game.java
+++ Game.java
@@ -20,7 +20,8 @@ public class Game
     {
         // initialise instance variables
         this.players = players;
-        numRounds = (int)(Math.random() * 101) + 100; //SET TO 50 FOR TESTING, WILL BE RANDOM 100 - 200
+        //numRounds = (int)(Math.random() * 101) + 100; //SET TO 50 FOR TESTING, WILL BE RANDOM 100 - 200
+        numRounds = 200;
         score1 = score2 = 0;
         this.print = print;
         scores = new int [players.length];
--- PaluriSrikar.java
+++ PaluriSrikar.java
@@ -1,23 +1,23 @@
 import java.util.*;
 /**
  * Each player or team will create their own Player, this is only an example....
- * 
+ *
  * Your code MUST INCLUDE a move method with the following signature. To insure that, you must implement
  * the Player interface
- * 
+ *
  * You can create additional methods but must have the move method
- * 
+ *
  */
 public class PaluriSrikar implements Player
 {
     private static String name = "Srikar Paluri";
-    private int numberOfRsPending =0; //number of Rs to be counted before sending s
-    private int currentCountOfRs =0;//keep track of current number of Rs
+    private int numberOfRsPending =1; //number of Rs to be counted before sending s
+    private int currentCountOfRs =1;//keep track of current number of Rs
     /**
      * An example of a method - replace this comment with your own
      * You must create some kind of logic of what to play against your opponent...start researching,
      * driver code to be provided MONDAY, 2/27
-     * 
+     *
      * @return      the move you want to play against opponent
      *              "r" - rock
      *              "p" - paper
@@ -26,16 +26,20 @@ public class PaluriSrikar implements Player
      */
     public String move(String [] myMoves, String [] opponentMoves, int myScore, int opponentScore, int round)
     {
-       int  rand = (int)(Math.random() * 100) % 3;
-       if ( rand == 1){
-           numberOfRsPending =2;
-           currentCountOfRs =2;
-           return "s";
-        }
-        else if ( numberOfRsPending > 0 ) {
+        //if ( round == 1){
+          // return "p";
+           
+        //}
+        //else if ( round == 2 ){
+           
+        //   numberOfRsPending =2;
+         //  currentCountOfRs =2;
+         //  return "s";
+        //}
+         if ( numberOfRsPending > 0 ) {
            numberOfRsPending--;
            return "p";
-       } 
+       }
        else {
            currentCountOfRs++;
            numberOfRsPending = currentCountOfRs;
@@ -44,7 +48,7 @@ public class PaluriSrikar implements Player
     }
     /**
      * Returns the name of the player
-     * 
+     *
      * @return      the name of the player              
      */
     public String getName()
